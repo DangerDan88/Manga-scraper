b260fa76e6d7ba5332955b8c96ddb5eed043786f
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-08-17 11:04:27 -0600

    getting cors error when trying to scrape from barnes and noble may need to be a command line program

---
+++ index.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+//import cheerio from "cheerio";
+// cross scripting error when trying to pull from barnes and noble?
+const url = "https://www.nfl.com/stats/player-stats/category/rushing/2020";
+axios(url)
+  .then((response) => {
+    const data = response.data;
+    //  const $ = cheerio.load(data);
+    console.log(data);
+  })
+  .catch(console.error);
+console.log("linked");
