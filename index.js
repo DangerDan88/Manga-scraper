53a195d651f2543fdd8fcde3a73db70c995887ff
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-08-22 11:15:06 -0600

    switching to only node scraper to get rid of cors error

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-// cross scripting error when trying to pull from barnes and noble?
-const url = "https://www.nfl.com/stats/player-stats/category/rushing/2020";
+// had to switch to node for no Cors error got data to come thru need to try it on book sites
+const url = "https://www.nfl.com/stats/player-stats/";
 axios(url)
   .then((response) => {
     const data = response.data;
     const $ = cheerio.load(data);
+    let test = $`(<p>  </p>)`;
+    test.innerHtml = data;
     console.log(data);
   })
   .catch(console.error);
