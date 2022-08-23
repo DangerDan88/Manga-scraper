cd3a64c4c794f9851cd82fbfafb3e5b586596e4c
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-08-23 10:59:05 -0600

    running local server for this to not run into cors error

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import axios from "axios";
-import * as cheerio from "cheerio";
-// had to switch to node for no Cors error got data to come thru need to try it on book sites
-const url = "https://www.nfl.com/stats/player-stats/";
-axios(url)
-  .then((response) => {
-    const data = response.data;
-    const $ = cheerio.load(data);
-    let test = $`(<p>  </p>)`;
-    test.innerHtml = data;
-    console.log(data);
-  })
-  .catch(console.error);
-console.log("linked");
+//import axios from "axios";
+//import * as cheerio from "cheerio";
+const http = require("http");
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at PORT:${PORT}/`);
+});
