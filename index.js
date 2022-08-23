c6791d58348d112405c6acbab8bb0d99e588e131
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-08-23 11:09:22 -0600

    got local port open and some test data need to try and scrape from page I need next

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-//import axios from "axios";
 //import * as cheerio from "cheerio";
-const http = require("http");
+import http from "http";
 const PORT = 3000;
+import axios from "axios";
 
+let url = "https://www.reddit.com/r/programming.json";
+
+axios(url)
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
