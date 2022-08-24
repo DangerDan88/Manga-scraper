a87cc775073c482274db7a81aba04ed1ab683d36
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-08-24 11:10:24 -0600

    need to add a db to pull scraped data to html page

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ import axios from "axios";
 const PORT = 3000;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
 app.get("/", function (req, res) {
   res.send("Hello world!");
 });
 
 let url = "https://www.barnesandnoble.com/b/viz-media/_/N-1p70";
-// might need to install express here not entirley sure yet
+// I think I need to add a DB so when we scrape we then can render that after pulling it from DB to html page
+// need to look into options for DB would rather not use mongo or SQL
 axios(url)
   .then((response) => {
     const $ = cheerio.load(response.data);
-    console.log($);
+    //console.log($);
     $(".product-shelf-title").each(function (i, element) {
       const result = {};
       result.title = $(this).children("a").text();
-      // result.link = $(this).children("a").attr("href");
+      result.link = $(this).children("a").attr("href");
       console.log(result);
     });
   })
