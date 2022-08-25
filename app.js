ab45acb5d8c625ceb6dc6cf86dc8daf9b74fb7e9
Danger <51889207+DangerDan88@users.noreply.github.com>
2022-08-25 10:28:15 -0600

    trying to get exphbs to work I think its not working since I have no data to render might need DB before

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ import * as cheerio from "cheerio";
 import express from "express";
 import axios from "axios";
 const PORT = 3000;
+import exphbs from "express-handlebars";
+//import { engine } from "express-handlebars";
 
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./views");
-
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
-app.get("/", function (req, res) {
-  res.send("Hello world!");
-});
 
+// need to figure out why express handlebars is not working on import
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// Make public a static folder
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+/////////
+// getting an error failed to lookup main in views? Need to fix folder structure
 let url = "https://www.barnesandnoble.com/b/viz-media/_/N-1p70";
-// I think I need to add a DB so when we scrape we then can render that after pulling it from DB to html page
 // need to look into options for DB would rather not use mongo or SQL
+// need to work to clean up what I scrape once I can get it on the page
 axios(url)
   .then((response) => {
     const $ = cheerio.load(response.data);
@@ -33,4 +38,3 @@ axios(url)
   .catch((error) => {
     console.error(error);
   });
-// got some actual data to scrape need to clean up what scrapes then get it on page
